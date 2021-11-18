4c9c19637f5a7fd2d099aa2d7c574e5c194c22d0
Michael Walker <walkerrepair255@yahoo.com>
2021-11-18 17:54:15 -0500

    formatting and examples

--- html-tutorial/js/dom-tutorial.js
+++ html-tutorial/js/dom-tutorial.js
@@ -1,38 +1,38 @@
 class Person {
-    name;
-    color;
-    number;
+  name;
+  color;
+  number;
 
-    constructor(name, color, number) {
-        this.name = name;
-        this.color = color;
-        this.number = number;
-    }
-    toTableRow() {
-        let tr = "<tr>";
-        tr += `<td>${this.name}</td>`;
-        tr += `<td>${this.color}</td>`;
-        tr += `<td>${this.number}</td>`;
-        tr += "</tr>";
-        return tr;
-    }
+  constructor(name, color, number) {
+    this.name = name;
+    this.color = color;
+    this.number = number;
+  }
+  toTableRow() {
+    let tr = "<tr>";
+    tr += `<td>${this.name}</td>`;
+    tr += `<td>${this.color}</td>`;
+    tr += `<td>${this.number}</td>`;
+    tr += "</tr>";
+    return tr;
+  }
 }
 
-let dbDataClass = [
-    new Person("Scott", "blue", 4),
-    new Person("Todd", "green", 2),
-    new Person("Matt", "black", 3),
-    new Person("Ash", "brown", 6),
-    new Person("Fred", "gray", 7),
-    new Person("Patty", "pink", 9)
-]
+let persons = [
+  new Person("Scott", "blue", 4),
+  new Person("Todd", "green", 2),
+  new Person("Matt", "black", 3),
+  new Person("Ash", "brown", 6),
+  new Person("Fred", "gray", 7),
+  new Person("Patty", "pink", 9),
+];
 const loaded = () => {
-    let tbody = document.getElementById("tbody");
-    for(let row of dbDataClass) {
-        let tr = row.toTableRow();
-        tbody.innerHTML += tr;
-    }
-}
+  let tbody = document.getElementById("tbody");
+  for (let person of persons) {
+    let tr = row.toTableRow();
+    tbody.innerHTML += tr;
+  }
+};
 /*const loaded = () => {
     let tbody = document.getElementById("tbody");
     for(let row of dbData) {
@@ -55,52 +55,52 @@ const loaded = () => {
 
 ]*/
 const add = () => {
-    let operanda = +document.getElementById("opa").value;
-    let operandb = +document.getElementById("opb").value;
-    let answer = operanda + operandb;
-    document.getElementById("ans").value = answer;
-    document.getElementById("ans").style.color="black";
-    document.getElementById("lbl").innerHTML = "<b>Addition</b>";
-    document.getElementById("lbl").style.color="black";
-    console.log(answer);
-}
+  let operanda = +document.getElementById("opa").value;
+  let operandb = +document.getElementById("opb").value;
+  let answer = operanda + operandb;
+  document.getElementById("ans").value = answer;
+  document.getElementById("ans").style.color = "black";
+  document.getElementById("lbl").innerHTML = "<b>Addition</b>";
+  document.getElementById("lbl").style.color = "black";
+  console.log(answer);
+};
 const sub = () => {
-    let operanda = +document.getElementById("opa").value;
-    let operandb = +document.getElementById("opb").value;
-    let answer = operanda - operandb;
-    document.getElementById("ans").value = answer;
-    document.getElementById("ans").style.color="red";
-    document.getElementById("lbl").innerText = "Subtraction";
-    document.getElementById("lbl").style.color="red";
-    console.log(answer);
-}
+  let operanda = +document.getElementById("opa").value;
+  let operandb = +document.getElementById("opb").value;
+  let answer = operanda - operandb;
+  document.getElementById("ans").value = answer;
+  document.getElementById("ans").style.color = "red";
+  document.getElementById("lbl").innerText = "Subtraction";
+  document.getElementById("lbl").style.color = "red";
+  console.log(answer);
+};
 const multi = () => {
-    let operanda = +document.getElementById("opa").value;
-    let operandb = +document.getElementById("opb").value;
-    let answer = operanda * operandb;
-    document.getElementById("ans").value = answer;
-    document.getElementById("ans").style.color="green";
-    document.getElementById("lbl").innerText = "Mutiplication";
-    document.getElementById("lbl").style.color="green";
-    console.log(answer);
-}
+  let operanda = +document.getElementById("opa").value;
+  let operandb = +document.getElementById("opb").value;
+  let answer = operanda * operandb;
+  document.getElementById("ans").value = answer;
+  document.getElementById("ans").style.color = "green";
+  document.getElementById("lbl").innerText = "Mutiplication";
+  document.getElementById("lbl").style.color = "green";
+  console.log(answer);
+};
 const div = () => {
-    let operanda = +document.getElementById("opa").value;
-    let operandb = +document.getElementById("opb").value;
-    let answer = operanda / operandb;
-    document.getElementById("ans").value = answer;
-    document.getElementById("ans").style.color="blue";
-    document.getElementById("lbl").innerText = "Divition";
-    document.getElementById("lbl").style.color="blue";
-    console.log(answer);
-}
+  let operanda = +document.getElementById("opa").value;
+  let operandb = +document.getElementById("opb").value;
+  let answer = operanda / operandb;
+  document.getElementById("ans").value = answer;
+  document.getElementById("ans").style.color = "blue";
+  document.getElementById("lbl").innerText = "Divition";
+  document.getElementById("lbl").style.color = "blue";
+  console.log(answer);
+};
 const rim = () => {
-    let operanda = +document.getElementById("opa").value;
-    let operandb = +document.getElementById("opb").value;
-    let answer = operanda % operandb;
-    document.getElementById("ans").value = answer;
-    document.getElementById("ans").style.color="purple";
-    document.getElementById("lbl").innerText = "Remainder";
-    document.getElementById("lbl").style.color="purple";
-    console.log(answer);
-}
+  let operanda = +document.getElementById("opa").value;
+  let operandb = +document.getElementById("opb").value;
+  let answer = operanda % operandb;
+  document.getElementById("ans").value = answer;
+  document.getElementById("ans").style.color = "purple";
+  document.getElementById("lbl").innerText = "Remainder";
+  document.getElementById("lbl").style.color = "purple";
+  console.log(answer);
+};
